65e110458f1514e86dea7f64773abbbbbbfd3655
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-23 04:26:57 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-        if attrinbute == :favorite_ice_cream_flavors
+        if attribute == :favorite_ice_cream_flavors
            data.delete_if {|icr_cream| ice_cream == "strawberry"}
         end 
       end 
